67c5418c5dd4849bfe433f38d24380d485e05a05
pyranth <pyranth95@hotmail.com>
2018-05-27 23:30:55 +0200

    Added db config

---
+++ webapp/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+$con = mysqli_connect("localhost","paviljon_kmet","kmetkmet","paviljon_kmetovi");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else
+  {
+	  echo "Connection success!";
+  }
+?>
