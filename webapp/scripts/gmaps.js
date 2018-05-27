992a3204f32e11ec4a286d4668867eeb0e919af8
pyranth <pyranth95@hotmail.com>
2018-05-28 01:53:45 +0200

    Typos

--- webapp/scripts/gmaps.js
+++ webapp/scripts/gmaps.js
@@ -11,7 +11,7 @@ function myMap() {
 
     var mapOptions = {
         center: new google.maps.LatLng(43.3555541,17.8074431),
-        zoom: 17,
+        zoom: 15,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         styles: myStyles 
     }
@@ -22,7 +22,7 @@ function myMap() {
         title: "Test marker"
     });
 
-    var contentString = '<p>Bank 1</p><p>People in queue: 7<p><p>Average waiting time: 17min</p>';
+    var contentString = '<p>Bank 1</p><p>People in queue: 7<p><p>Estimated waiting time: 17min</p>';
 
     var infowindow = new google.maps.InfoWindow({
         content: contentString
@@ -32,6 +32,6 @@ function myMap() {
         infowindow.open(map, marker);
       });    
     
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-marker.setMap(map);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    marker.setMap(map);
 } 
