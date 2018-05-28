d68dc438e2bf1f3e3678e5908a1c9dd694185fe2
pyranth <pyranth95@hotmail.com>
2018-05-29 01:04:53 +0200

    Api for update

---
+++ webapp/api/update-stanje-trenutno.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../dbconfig.php';
+
+$query =    "SELECT * FROM STANJE 
+            WHERE ID_USTANOVE=" . $_GET['id_ustanove'] .
+            " AND DATUM='" . date('Y-m-d') . "';";
+
+$result = mysqli_query($con, $query);
+$array = array();
+
+while($row = mysqli_fetch_assoc($result)) {
+    array_push($array, $row);
+}
+
+if (empty($array) == TRUE) 
+{
+    $query =    "INSERT INTO STANJE(ID_USTANOVE, TRENUTNO_STANJE, POSLEDNJI_UZETI, DATUM)
+                VALUES(" . $_GET['id_ustanove'] . ", 0, 0, '" . date('Y-m-d') . "');";
+
+    $con->query($query);
+}
+
+$query =    "UPDATE STANJE 
+            SET TRENUTNO_STANJE=" . $_GET['trenutno_stanje'] . 
+            " WHERE ID_USTANOVE=" . $_GET['id_ustanove'] . 
+            " AND DATUM='" . date('Y-m-d') . "';";
+
+if ($con->query($query) == TRUE)
+{
+    echo "Record updated successfuly";
+}
+else
+{
+    echo "Error updating record: " . $con->error;
+}
+
+?>
